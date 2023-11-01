cc4e7e0c38f17d0db7c6784bc3db08be7b8cb9d5
Диана Амирханова <dinaami187@gmail.com>
2023-11-01 10:52:49 +0000

    user&admin

--- gardens/gardens/settings.py
+++ gardens/gardens/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS += [
 #applications
 INSTALLED_APPS += [
     'garden.apps.GardenConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -112,6 +113,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
